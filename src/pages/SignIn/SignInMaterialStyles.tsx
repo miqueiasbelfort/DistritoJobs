500cd760538b6b8d39598a39c1b2a9afa93011ef
Miqueias Kawa <kawasousa1554@gmail.com>
2023-07-27 21:45:39 -0300

    add Material UI

---
+++ src/pages/SignIn/SignInMaterialStyles.tsx
@@ -0,0 +1,8 @@
+import { Container, styled } from '@mui/material';
+
+export const ContainerCenter = styled(Container)(() => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh'
+}));
